5c343893bce702b001803673c77a3cff4874223d
ameerthehacker <ameerjhanprof@gmail.com>
2018-07-26 01:09:05 +0530

    Add integration tests

--- src/app/components/todo-form/todo-form.component.spec.ts
+++ src/app/components/todo-form/todo-form.component.spec.ts
@@ -5,6 +5,7 @@ import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { TodoFormComponent } from './todo-form.component';
 
 import { TodoService } from '../../services/todo.service';
+import { By } from '@angular/platform-browser';
 
 class RouterStub {
   navigate(params) {}
@@ -13,6 +14,7 @@ class RouterStub {
 describe('TodoFormComponent', () => {
   let component: TodoFormComponent;
   let fixture: ComponentFixture<TodoFormComponent>;
+  let newTodo: string;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -32,10 +34,36 @@ describe('TodoFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(TodoFormComponent);
     component = fixture.componentInstance;
+    // Arrange
+    newTodo = 'new todo!';
+    component.formGroup.get("todo").setValue(newTodo);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call todoService.addTodo on save clicked', () => {
+    // Arrange
+    const todoService = TestBed.get(TodoService);
+    spyOn(todoService, 'addTodo');
+    // Act
+    component.onBtnSave();
+    // Assert
+    expect(todoService.addTodo).toHaveBeenCalledWith(newTodo);
+  });
+
+  it('should navigate to / on save clicked', () => {
+    // Arrange
+    const router = TestBed.get(Router);
+    component.formGroup.get('todo').setValue(newTodo);
+    spyOn(router, 'navigate');
+    // Act
+    // Act
+    component.onBtnSave();
+    // Assert
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
+
   });
 });
